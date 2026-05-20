1dd0b064edf258307a349481b6b5102dbcd6f7c6
rohithkumardharamkar <rohithkumardharamkar@gmail.com>
2026-05-20 16:00:35 +0530

    updated the service for loading the Groq api

--- services/llm.py
+++ services/llm.py
@@ -1,5 +1,10 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
-llm=ChatGroq(model_name='llama-3.3-70b-versatile')
+
+llm = ChatGroq(
+    api_key=os.getenv("GROQ_API_KEY"),
+    model="llama-3.3-70b-versatile"
+)
